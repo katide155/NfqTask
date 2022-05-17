7fd80423f7781ea9e14e0326fc1215daf7567dfa
katide155 <93394912+katide155@users.noreply.github.com>
2022-05-17 09:02:05 +0300

    Initial commit

---
+++ StudentsData/database/factories/StudentFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'student_name' => $this->faker->firstName(),
+			'student_surname' => $this->faker->lastName(),
+        ];
+    }
+}
